e45273d1e220ca15059ac95dfb0b82adcb5ecfcf
abraao <abraaocpm@hotmail.com>
2023-02-17 13:59:15 -0300

    alteracao axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 // const url = 'https://api-projeto-node.herokuapp.com/';
 // const url = 'http://192.16.1.209:3001';
-const url = 'http://192.168.0.186:3001';
+// const url = 'http://192.168.0.186:3001';
 // const url = 'http://localhost:3001';
+const url = 'https://apigincana.herokuapp.com/';
+
 export default axios.create({
   baseURL: url,
 });
