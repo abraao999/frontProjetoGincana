15cb71e38b6f0fc73e282a823f285e47583470ec
abraao <abraaocpm@hotmail.com>
2023-02-17 14:14:05 -0300

    lixo

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,8 @@ import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Page404 from '../pages/Page404';
-import EditPass from '../pages/AbaSecretaria/EditPass';
 
-import Configuracoes from '../pages/AbaConfiguracoes/Configuracoes';
 import Home from '../pages/Home';
-import CadCliente from '../pages/CadCliente';
 
 import CadPerguntas from '../pages/CadPergunta';
 import PainelPergunta from '../pages/PainelPergunta';
@@ -23,36 +20,16 @@ export default function Routes() {
       <MyRoute path="/home" component={Home} isClosed={false} />
 
       <MyRoute
-        path="/cadCliente/:id/:titulo"
-        component={CadCliente}
-        isClosed={false}
-      />
-      <MyRoute path="/cadCliente" component={CadCliente} isClosed={false} />
-      <MyRoute
-        path="/painelPergunta/:id/:titulo"
-        component={PainelPergunta}
-        isClosed={false}
-      />
-      <MyRoute
         path="/painelPergunta"
         component={PainelPergunta}
         isClosed={false}
       />
-
-      <MyRoute path="/configuracoes/" component={Configuracoes} isClosed />
 
       <MyRoute path="/login/" component={Login} isClosed={false} />
       <MyRoute path="/register/" component={Register} isClosed={false} />
       <MyRoute
         path="/cadPerguntas/"
         component={CadPerguntas}
-        isClosed={false}
-      />
-
-      <MyRoute
-        path="/editPass/"
-        component={EditPass}
-        usuarioPermitido={[{ id: 1 }, { id: 2 }, { id: 4 }]}
         isClosed={false}
       />
 
